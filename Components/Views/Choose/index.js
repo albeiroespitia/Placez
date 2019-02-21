826816a6f392033aacc6d28067ae9843d6be8939
ivanletralok <ivanletralok@gmail.com>
2019-02-21 16:50:35 -0500

    creando segunda vista

---
+++ Components/Views/Choose/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Container} from 'native-base'
+
+export default class Choose extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <Container>
+                
+            </Container>
+        )
+    }
+}
